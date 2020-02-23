043ca8526b5ac6abd04cf971c806f94aa270a410
Zackery Fortier <zackery.fortier@selu.edu>
2020-02-23 16:31:57 -0600

    Added css files and card components

--- monsters/src/App.js
+++ monsters/src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { CardList } from './components/card-list.component'
+import { CardList } from './components/card-list/card-list.component'
 import './App.css';
 
 class App extends Component {
@@ -7,7 +7,8 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     } 
   }
 
@@ -20,11 +21,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <CardList name="Zack"/>
-      
-        {
-          this.state.monsters.map(monster => <h1 key={monster.id}> { monster.name }</h1>)
-        }
+      <input type='search' placeholder='search monsters' onChange={e => console.log(e)}/>
+      <CardList monsters={this.state.monsters} />
       </div>
     );
   }
