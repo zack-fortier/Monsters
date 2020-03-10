3e63dd1437c9a6de889cd5f9b1a877da67446759
Zackery Fortier <zackery.fortier@selu.edu>
2020-03-09 20:42:53 -0500

    added searchfield

--- monsters/src/App.js
+++ monsters/src/App.js
@@ -21,7 +21,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <input type='search' placeholder='search monsters' onChange={e => console.log(e)}/>
+      <input type='search' 
+      placeholder='search monsters' 
+      onChange={e => this.setState({ searchField: e.target.value })} />
       <CardList monsters={this.state.monsters} />
       </div>
     );
