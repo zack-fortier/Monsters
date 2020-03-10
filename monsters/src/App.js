444a3c649fda9ac213c35bb553fee6320020c66c
Zackery Fortier <zackery.fortier@selu.edu>
2020-03-09 21:32:47 -0500

    Added search box component and styles

--- monsters/src/App.js
+++ monsters/src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import { CardList } from './components/card-list/card-list.component'
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 import './App.css';
 
 class App extends Component {
@@ -19,12 +20,16 @@ class App extends Component {
   }
   
   render() {
-    return (
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase()));
+    
+      return (
       <div className="App">
-      <input type='search' 
-      placeholder='search monsters' 
-      onChange={e => this.setState({ searchField: e.target.value })} />
-      <CardList monsters={this.state.monsters} />
+      <SearchBox 
+      placeholder='search monsters'
+      handleChange={e => this.setState({ searchField: e.target.value })} />
+      <CardList monsters={filteredMonsters} />
       </div>
     );
   }
