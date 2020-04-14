edfa41e145c2e6d425b3be31dbd822e356226e85
Zackery Fortier <zackery.fortier@selu.edu>
2020-04-13 20:22:27 -0500

    Updates

---
+++ precache-manifest.362c8ce1b49ab6473b2d3c7932a74461.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d824b65e78fe938af2057d06a933289b",
+    "url": "/Monsters/index.html"
+  },
+  {
+    "revision": "ebb5ef46db5ae5eecb3b",
+    "url": "/Monsters/static/css/main.326c3272.chunk.css"
+  },
+  {
+    "revision": "d5b7dc0e95c86de42d59",
+    "url": "/Monsters/static/js/2.0b9572cb.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Monsters/static/js/2.0b9572cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebb5ef46db5ae5eecb3b",
+    "url": "/Monsters/static/js/main.c63fed49.chunk.js"
+  },
+  {
+    "revision": "aa984bed772c375e8a1a",
+    "url": "/Monsters/static/js/runtime-main.7c2fcb8f.js"
+  }
+]);
